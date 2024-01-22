2bfb8ac8fbcb3bafbc478402eb9b8b92028ea89f
Ali <49570285+hasack@users.noreply.github.com>
2024-01-22 22:52:08 +0000

    Add project files.

---
+++ Console Infrastructure Queries/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Infrastructure_Queries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
